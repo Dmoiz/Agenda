1c788f7c5f17f10a02ddb61cce054938094bd01b
Diego Moreno <95129545+Dmoiz@users.noreply.github.com>
2023-01-09 20:16:13 +0100

    Initial Commit

---
+++ Agenda/AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaApp.swift
+//  Agenda
+//
+//  Created by Diego Moreno on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
